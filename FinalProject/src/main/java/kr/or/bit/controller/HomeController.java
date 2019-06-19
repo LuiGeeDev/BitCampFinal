a9c86c56efe2ba7a5d459088985d5928591d9b90
LuiGee3471 <yjs3475@gmail.com>
2019-06-19 14:07:10 +0900

    레이아웃 조정

--- FinalProject/src/main/java/kr/or/bit/controller/HomeController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/HomeController.java
@@ -28,76 +28,8 @@ public class HomeController {
   private SqlSession sqlsession;
 
   @GetMapping("/")
-  public String home(Model model, HttpSession session) {
-    BoardDao boardDao = sqlsession.getMapper(BoardDao.class);
-    List<Board> boardList = boardDao.getList();
-    
-    model.addAttribute("boardList", boardList);
+  public String home(Model model) {
+    model.addAttribute("message", "world");
     return "home";
-  }
-
-  @GetMapping("/reg")
-  public String reg() {
-    return "reg";
-  }
-
-  @PostMapping("/reg")
-  public String regOk(Member member) throws ClassNotFoundException, SQLException {
-    MemberDao memberDao = sqlsession.getMapper(MemberDao.class);
-    memberDao.join(member);
-
-    return "redirect:/";
-  }
-
-  @GetMapping("/login")
-  public String login() throws ClassNotFoundException, SQLException {
-    return "login";
-  }
-
-  @PostMapping("/loginOk")
-  public ModelAndView loginOk(Member member, HttpServletRequest request) throws ClassNotFoundException, SQLException {
-    MemberDao memberDao = sqlsession.getMapper(MemberDao.class);
-    ModelAndView mav = new ModelAndView();
-    
-    if (memberDao.selectMember(member.getId()).getPassword().equals(member.getPassword())) {
-      HttpSession session = request.getSession();
-      session.setAttribute("user", member);
-      mav.setViewName("redirect:/");
-    } else {
-      mav.setViewName("login");
-    }
-
-    return mav;
-  }
-  
-  @GetMapping("/board/{boardId}")
-  public String detail(@PathVariable int boardId, Model model, HttpSession session) {
-    if (session.getAttribute("user") == null) {
-      return "redirect:/";
-    }
-    BoardDao boardDao = sqlsession.getMapper(BoardDao.class);
-    Board board = boardDao.getBoard(boardId);
-    CommentDao commentDao = sqlsession.getMapper(CommentDao.class);
-    List<Comment> commentList = commentDao.getComments(boardId);
-
-    model.addAttribute("commentList", commentList);
-    model.addAttribute("board", board);
-    return "board";
-  }
-  
-  @PostMapping("/board/{boardId}")
-  public String writeComment(@PathVariable int boardId, Comment comment, HttpSession session) {
-    CommentDao commentDao = sqlsession.getMapper(CommentDao.class);
-    Member member = (Member) session.getAttribute("user");
-    comment.setWriter(member.getId());
-    commentDao.writeComment(comment);
-    
-    return "redirect:/board/" + boardId;
-  }
-  
-  @GetMapping("/logout")
-  public String logout(HttpSession session) {
-    session.invalidate();
-    return "redirect:/";
   }
 }
