c59244f2cdd22ff49ffd62d0220a1adbd9ed104c
LuiGee3471 <yjs3475@gmail.com>
2019-06-21 19:07:17 +0900

    로그인 암호화 구현

--- FinalProject/src/main/java/kr/or/bit/controller/HomeController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/HomeController.java
@@ -1,15 +1,17 @@
 package kr.or.bit.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import kr.or.bit.service.NewsService;
 
 @Controller
 public class HomeController {
   @GetMapping("/")
   public String home(Model model) {
+    UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    String username = userDetails.getUsername();
     return "main";
   }
 
