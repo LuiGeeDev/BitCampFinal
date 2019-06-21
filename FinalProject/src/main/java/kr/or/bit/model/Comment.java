d900ac85bdc78b2fd2773bccbc68dab98456510c
wjfjstkrtm3 <jsy7501@gmail.com>
2019-06-21 12:03:14 +0900

    DTO TIME 수정

--- FinalProject/src/main/java/kr/or/bit/model/Comment.java
+++ FinalProject/src/main/java/kr/or/bit/model/Comment.java
@@ -1,6 +1,7 @@
 package kr.or.bit.model;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class Comment {
   private int id;
@@ -8,38 +9,59 @@ public class Comment {
   private Timestamp time;
   private int article_id;
   private String username;
-  
+  private LocalDateTime localTime;
+
   public int getId() {
     return id;
   }
+
   public void setId(int id) {
     this.id = id;
   }
+
   public String getContent() {
     return content;
   }
+
   public void setContent(String content) {
     this.content = content;
   }
+
   public Timestamp getTime() {
     return time;
   }
+
   public void setTime(Timestamp time) {
     this.time = time;
   }
+
   public int getArticle_id() {
     return article_id;
   }
+
   public void setArticle_id(int article_id) {
     this.article_id = article_id;
   }
+
   public String getUsername() {
     return username;
   }
+
   public void setUsername(String username) {
     this.username = username;
   }
-  
-  
-  
+
+  public LocalDateTime getLocalTime() {
+    return localTime;
+  }
+
+  public void setLocalTime(LocalDateTime localTime) {
+    this.localTime = localTime;
+  }
+
+  @Override
+  public String toString() {
+    return "Comment [id=" + id + ", content=" + content + ", time=" + time + ", article_id=" + article_id
+        + ", username=" + username + ", localTime=" + localTime + "]";
+  }
 }
--- FinalProject/src/main/java/kr/or/bit/model/Schedule.java
+++ FinalProject/src/main/java/kr/or/bit/model/Schedule.java
@@ -1,58 +1,95 @@
 package kr.or.bit.model;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 public class Schedule {
-  
   private int id;
   private Date start_date;
   private Date end_date;
+  private LocalDate startDate;
+  private LocalDate endDate;
   private String content;
   private String color;
   private int group_id;
   private int course_id;
-  
+
   public int getId() {
     return id;
   }
+
   public void setId(int id) {
     this.id = id;
   }
+
   public Date getStart_date() {
     return start_date;
   }
+
   public void setStart_date(Date start_date) {
     this.start_date = start_date;
   }
+
   public Date getEnd_date() {
     return end_date;
   }
+
   public void setEnd_date(Date end_date) {
     this.end_date = end_date;
   }
+
+  public LocalDate getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(LocalDate startDate) {
+    this.startDate = startDate;
+  }
+
+  public LocalDate getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(LocalDate endDate) {
+    this.endDate = endDate;
+  }
+
   public String getContent() {
     return content;
   }
+
   public void setContent(String content) {
     this.content = content;
   }
+
   public String getColor() {
     return color;
   }
+
   public void setColor(String color) {
     this.color = color;
   }
+
   public int getGroup_id() {
     return group_id;
   }
+
   public void setGroup_id(int group_id) {
     this.group_id = group_id;
   }
+
   public int getCourse_id() {
     return course_id;
   }
+
   public void setCourse_id(int course_id) {
     this.course_id = course_id;
   }
-  
+
+  @Override
+  public String toString() {
+    return "Schedule [id=" + id + ", start_date=" + start_date + ", end_date=" + end_date + ", startDate=" + startDate
+        + ", endDate=" + endDate + ", content=" + content + ", color=" + color + ", group_id=" + group_id
+        + ", course_id=" + course_id + "]";
+  }
 }
