7c3715df6a02c156fab7a7e69121d35b0fedc7b1
wjfjstkrtm3 <jsy7501@gmail.com>
2019-06-21 14:55:47 +0900

    DAO 추가

--- FinalProject/src/main/java/kr/or/bit/dao/MessageDao.java
+++ FinalProject/src/main/java/kr/or/bit/dao/MessageDao.java
@@ -2,12 +2,25 @@ package kr.or.bit.dao;
 
 import kr.or.bit.model.Message;
 
+/*
+*
+* @date: 2019. 06. 21.
+*
+* @author: 정성윤
+*
+* @description: MessageDao의 CRUD
+*
+*/
 public interface MessageDao {
   void insertMessage(Message message);
+
   void deleteMessage(int id);
+
   void selectAllMessage(String receiver_username);
+
   void selectIdMessage(String sender_username);
+
   void selectOldMessage(String receiver_username);
+
   void selectNewMessage(String receiver_username);
-  
 }
--- FinalProject/src/main/java/kr/or/bit/dao/NotificationDao.java
+++ FinalProject/src/main/java/kr/or/bit/dao/NotificationDao.java
@@ -2,11 +2,25 @@ package kr.or.bit.dao;
 
 import kr.or.bit.model.Notification;
 
+/*
+*
+* @date: 2019. 06. 21.
+*
+* @author: 정성윤
+*
+* @description: Notification의 CRUD
+*
+*/
 public interface NotificationDao {
   void insertNotification(Notification Notification);
+
   void updateNotification(int checked);
+
   void deleteNotification(int id);
+
   void selectAllNotification(int id);
+
   void selectAllNewNotification(int id);
+
   void selectAllOldNotification(int id);
 }
