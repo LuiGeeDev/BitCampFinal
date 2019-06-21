80730b2cfb0dfc21837d6367f3da9654c2638928
이힘찬 <glacks4871@gmail.com>
2019-06-21 12:04:29 +0900

    Member 수정

--- FinalProject/src/main/java/kr/or/bit/model/Member.java
+++ FinalProject/src/main/java/kr/or/bit/model/Member.java
@@ -16,54 +16,71 @@ public class Member {
   public String getUsername() {
     return username;
   }
+
   public void setUsername(String username) {
     this.username = username;
   }
+
   public String getPassword() {
     return password;
   }
+
   public void setPassword(String password) {
     this.password = password;
   }
+
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
+
   public String getEmail() {
     return email;
   }
+
   public void setEmail(String email) {
     this.email = email;
   }
+
   public int getEnabled() {
     return enabled;
   }
+
   public void setEnabled(int enabled) {
     this.enabled = enabled;
   }
+
   public int getCourse_id() {
     return course_id;
   }
+
   public void setCourse_id(int course_id) {
     this.course_id = course_id;
   }
+
   public String getProfile_photo() {
     return profile_photo;
   }
+
   public void setProfile_photo(String profile_photo) {
     this.profile_photo = profile_photo;
   }
+
   public String getRole() {
     return role;
   }
+
   public void setRole(String role) {
     this.role = role;
   }
+
   public List<String> getScrap() {
     return scrap;
   }
+
   public void setScrap(List<String> scrap) {
     this.scrap = scrap;
   }
