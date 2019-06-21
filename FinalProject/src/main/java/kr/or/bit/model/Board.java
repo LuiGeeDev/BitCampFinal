90228d9ddf2b1e0e0a8666ce8879daf269818cf4
이힘찬 <glacks4871@gmail.com>
2019-06-21 11:23:54 +0900

    Board,Comment,Member,Message,Timeline - 수정

--- FinalProject/src/main/java/kr/or/bit/model/Board.java
+++ FinalProject/src/main/java/kr/or/bit/model/Board.java
@@ -5,36 +5,45 @@ public class Board {
   private String board_name;
   private int boardtype;
   private int course_id;
-  private String typename;
-  
+  private String typeName;
+
   public int getId() {
     return id;
   }
+
   public void setId(int id) {
     this.id = id;
   }
+
   public String getBoard_name() {
     return board_name;
   }
+
   public void setBoard_name(String board_name) {
     this.board_name = board_name;
   }
+
   public int getBoardtype() {
     return boardtype;
   }
+
   public void setBoardtype(int boardtype) {
     this.boardtype = boardtype;
   }
+
   public int getCourse_id() {
     return course_id;
   }
+
   public void setCourse_id(int course_id) {
     this.course_id = course_id;
   }
-  public String getTypename() {
-    return typename;
+
+  public String getTypeName() {
+    return typeName;
   }
-  public void setTypename(String typename) {
-    this.typename = typename;
+
+  public void setTypeName(String typeName) {
+    this.typeName = typeName;
   }
 }
--- FinalProject/src/main/java/kr/or/bit/model/Comment.java
+++ FinalProject/src/main/java/kr/or/bit/model/Comment.java
@@ -1,42 +1,79 @@
 package kr.or.bit.model;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 public class Comment {
   private int id;
   private String content;
-  private Date time;
   private int article_id;
   private String username;
-  
+  private Date time;
+  private Date updatedTime;
+  private LocalDate timeLocal;
+  private LocalDate updatedTimeLocal;
+
   public int getId() {
     return id;
   }
+
   public void setId(int id) {
     this.id = id;
   }
+
   public String getContent() {
     return content;
   }
+
   public void setContent(String content) {
     this.content = content;
   }
-  public Date getTime() {
-    return time;
-  }
-  public void setTime(Date time) {
-    this.time = time;
-  }
+
   public int getArticle_id() {
     return article_id;
   }
+
   public void setArticle_id(int article_id) {
     this.article_id = article_id;
   }
+
   public String getUsername() {
     return username;
   }
+
   public void setUsername(String username) {
     this.username = username;
+  }
+
+  public Date getTime() {
+    return time;
+  }
+
+  public void setTime(Date time) {
+    this.time = time;
+  }
+
+  public Date getUpdatedTime() {
+    return updatedTime;
+  }
+
+  public void setUpdatedTime(Date updatedTime) {
+    this.updatedTime = updatedTime;
+  }
+
+  public LocalDate getTimeLocal() {
+    return timeLocal;
+  }
+
+  public void setTimeLocal(LocalDate timeLocal) {
+    this.timeLocal = timeLocal;
+  }
+
+  public LocalDate getUpdatedTimeLocal() {
+    return updatedTimeLocal;
+  }
+
+  public void setUpdatedTimeLocal(LocalDate updatedTimeLocal) {
+    this.updatedTimeLocal = updatedTimeLocal;
   }
 }
