9d405ec6a585c278bc29776d3e7e67ac78e7a471
LuiGee3471 <yjs3475@gmail.com>
2019-06-21 16:06:18 +0900

    JsonView 설정

--- FinalProject/src/main/java/kr/or/bit/controller/AjaxController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/AjaxController.java
@@ -1,20 +1,26 @@
 package kr.or.bit.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.View;
 
 import kr.or.bit.service.NewsService;
 
 @Controller
 @RequestMapping("/ajax")
 public class AjaxController {
+  @Autowired
+  private View jsonView;
+  
   @PostMapping("/news")
-  public @ResponseBody String getNews() {
+  public View getNews(Model model) {
     NewsService service = new NewsService();
     String news = service.getNews();
+    model.addAttribute("news", news);
     
-    return news;
+    return jsonView;
   }
 }
--- FinalProject/src/main/java/kr/or/bit/controller/HomeController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/HomeController.java
@@ -1,56 +1,20 @@
 package kr.or.bit.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.or.bit.service.NewsService;
 
 @Controller
 public class HomeController {
-  @Autowired
-  private SqlSession sqlsession;
-
   @GetMapping("/")
   public String home(Model model) {
-    model.addAttribute("message", "world");
     return "main";
   }
 
   @GetMapping("/menu")
-  public String menu(Model model) {
-    model.addAttribute("message", "world");
+  public String menu() {
     return "menu";
-  }
-  
-  @GetMapping("/login")
-  public String login() {
-    return "login";
-  }
-  
-  @GetMapping("/login-error")
-  public String loginFailed(Model model) {
-    model.addAttribute("error", true);
-    return "login";
-  }
-  
-  @GetMapping("/logout")
-  public String logout(HttpServletRequest request, HttpServletResponse response) {
-    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-    if (auth != null) {
-      new SecurityContextLogoutHandler().logout(request, response, auth);
-    }
-    
-    return "redirect:login";
   }
 }
