0547edaa847af5a676499f14a23d700d908e9561
mhgyj0916 <mhgyj0916@gmail.com>
2019-06-21 10:45:33 +0900

    Article,Group,Schedule,Vote,Option dto 추가

---
+++ FinalProject/src/main/java/kr/or/bit/model/Article.java
@@ -0,0 +1,101 @@
+package kr.or.bit.model;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+public class Article {
+  private int id;
+  private String title;
+  private String content;
+  private Timestamp time;
+  private Date update_time;
+  private int view_count;
+  private int original_id;
+  private int level;
+  private String username;
+  private int board_id;
+  private Member write;
+  private List<String> tags;
+  private ArticleOption option;
+  
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public String getContent() {
+    return content;
+  }
+  public void setContent(String content) {
+    this.content = content;
+  }
+  public Timestamp getTime() {
+    return time;
+  }
+  public void setTime(Timestamp time) {
+    this.time = time;
+  }
+  public Date getUpdate_time() {
+    return update_time;
+  }
+  public void setUpdate_time(Date update_time) {
+    this.update_time = update_time;
+  }
+  public int getView_count() {
+    return view_count;
+  }
+  public void setView_count(int view_count) {
+    this.view_count = view_count;
+  }
+  public int getOriginal_id() {
+    return original_id;
+  }
+  public void setOriginal_id(int original_id) {
+    this.original_id = original_id;
+  }
+  public int getLevel() {
+    return level;
+  }
+  public void setLevel(int level) {
+    this.level = level;
+  }
+  public String getUsername() {
+    return username;
+  }
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  public int getBoard_id() {
+    return board_id;
+  }
+  public void setBoard_id(int board_id) {
+    this.board_id = board_id;
+  }
+  public Member getWrite() {
+    return write;
+  }
+  public void setWrite(Member write) {
+    this.write = write;
+  }
+  public List<String> getTags() {
+    return tags;
+  }
+  public void setTags(List<String> tags) {
+    this.tags = tags;
+  }
+  public ArticleOption getOption() {
+    return option;
+  }
+  public void setOption(ArticleOption option) {
+    this.option = option;
+  }
+  
+}
---
+++ FinalProject/src/main/java/kr/or/bit/model/ArticleOption.java
@@ -0,0 +1,4 @@
+package kr.or.bit.model;
+
+public interface ArticleOption {
+}
