e6cc129bccffd03437e23170d04df3acab476e81
LuiGee3471 <yjs3475@gmail.com>
2019-06-21 14:23:24 +0900

    로그인 기능 구현
    - bituser/1004로 현재 로그인 가능

--- FinalProject/src/main/java/kr/or/bit/controller/HomeController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/HomeController.java
@@ -1,7 +1,13 @@
 package kr.or.bit.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,12 +26,31 @@ public class HomeController {
     model.addAttribute("message", "world");
     return "main";
   }
-  
+
   @GetMapping("/menu")
   public String menu(Model model) {
     model.addAttribute("message", "world");
     return "menu";
   }
   
-
+  @GetMapping("/login")
+  public String login() {
+    return "login";
+  }
+  
+  @GetMapping("/login-error")
+  public String loginFailed(Model model) {
+    model.addAttribute("error", true);
+    return "login";
+  }
+  
+  @GetMapping("/logout")
+  public String logout(HttpServletRequest request, HttpServletResponse response) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth != null) {
+      new SecurityContextLogoutHandler().logout(request, response, auth);
+    }
+    
+    return "redirect:login";
+  }
 }
