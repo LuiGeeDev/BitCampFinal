c9d76605c621ea538e5166cdbb6e192b918fd995
BokDaBeen <ekqlszzzz@naver.com>
2019-06-24 14:50:39 +0900

    Update Axios

--- FinalProject/src/main/java/kr/or/bit/controller/AxiosContoller.java
+++ FinalProject/src/main/java/kr/or/bit/controller/AxiosContoller.java
@@ -1,15 +1,27 @@
 package kr.or.bit.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+
+import kr.or.bit.model.Vote;
 
 @Controller
 @RequestMapping("/axios")
 public class AxiosContoller {
-
+	@Autowired
+	private View jsonView;
+	
 	@GetMapping("/addTodo")
-	public void addTodo() {
+	public View addTodo(Model model) {
 		System.out.println("애드투두");
+		Vote vote = new Vote();
+		vote.setComment_id(1);
+		vote.setUsername("권순조");
+		model.addAttribute("vote",vote);
+		return jsonView;
 	}
 }
