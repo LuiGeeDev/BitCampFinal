8d1f4d718e9591e5825be51d6ffdac1d9f31ddfb
mhgyj0916 <mhgyj0916@gmail.com>
2019-06-25 13:42:14 +0900

    CourseController에 insert 추가

--- FinalProject/src/main/java/kr/or/bit/controller/CourseController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/CourseController.java
@@ -2,11 +2,14 @@ package kr.or.bit.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.or.bit.dao.CourseDao;
 import kr.or.bit.dao.MessageDao;
@@ -19,9 +22,9 @@ public class CourseController {
   @Autowired
   private SqlSession sqlsession;
   
+  //강의목록 출력
   @RequestMapping("/course")
   public String selectTest(Model model) {
-    
     CourseDao coursedao = sqlsession.getMapper(CourseDao.class);    
     List<Course> list = coursedao.selectAllCourse();
     model.addAttribute("list", list);
@@ -29,17 +32,19 @@ public class CourseController {
     return "course";
   }
   
-  @RequestMapping("/course.insert")
+  //insert 페이지로 이동
+  @RequestMapping("/courseform")
   public String selectTest2(Model model) {
-    
-    CourseDao coursedao = sqlsession.getMapper(CourseDao.class);    
-    List<Course> list = coursedao.selectAllCourse();
-    model.addAttribute("list", list);
-    
-    return "course";
+    return "courseform";
   }
   
- 
+  //insert 제출 - 테스트용임
+  @RequestMapping("/courseinsert")
+  public String selectTest3(Course course) {
+    CourseDao coursedao = sqlsession.getMapper(CourseDao.class);  
+    coursedao.insertCourse(course);
+    return "course";
+  }
   
   
 }
