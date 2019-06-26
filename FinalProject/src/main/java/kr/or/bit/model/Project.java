e543b8ab7736c320776de3e3755d8d2935f8677e
이힘찬 <glacks4871@gmail.com>
2019-06-26 20:35:53 +0900

    프로젝트생성하기 진행중

--- FinalProject/src/main/java/kr/or/bit/model/Project.java
+++ FinalProject/src/main/java/kr/or/bit/model/Project.java
@@ -9,71 +9,56 @@ public class Project {
   private Date start_date;
   private Date end_date;
   private int season;
-  private int class_id;
+  private int course_id;
   
   private LocalDate startDateLocal;
   private LocalDate endDateLocal;
-
+  
   public int getId() {
     return id;
   }
-
   public void setId(int id) {
     this.id = id;
   }
-
   public String getProject_name() {
     return project_name;
   }
-
   public void setProject_name(String project_name) {
     this.project_name = project_name;
   }
-
   public Date getStart_date() {
     return start_date;
   }
-
   public void setStart_date(Date start_date) {
     this.start_date = start_date;
   }
-
   public Date getEnd_date() {
     return end_date;
   }
-
   public void setEnd_date(Date end_date) {
     this.end_date = end_date;
   }
-
   public int getSeason() {
     return season;
   }
-
   public void setSeason(int season) {
     this.season = season;
   }
-
-  public int getClass_id() {
-    return class_id;
+  public int getCourse_id() {
+    return course_id;
   }
-
-  public void setClass_id(int class_id) {
-    this.class_id = class_id;
+  public void setCourse_id(int course_id) {
+    this.course_id = course_id;
   }
-
   public LocalDate getStartDateLocal() {
     return startDateLocal;
   }
-
   public void setStartDateLocal(LocalDate startDateLocal) {
     this.startDateLocal = startDateLocal;
   }
-
   public LocalDate getEndDateLocal() {
     return endDateLocal;
   }
-
   public void setEndDateLocal(LocalDate endDateLocal) {
     this.endDateLocal = endDateLocal;
   }
