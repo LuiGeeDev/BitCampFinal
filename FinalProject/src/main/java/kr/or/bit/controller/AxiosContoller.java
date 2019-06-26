ba704c4eb6509ce5d9fa22c52894d2e2d7eb4894
LuiGee3471 <yjs3475@gmail.com>
2019-06-26 10:37:56 +0900

    TransactionManager 수정
    안됐음

--- FinalProject/src/main/java/kr/or/bit/controller/AxiosContoller.java
+++ FinalProject/src/main/java/kr/or/bit/controller/AxiosContoller.java
@@ -1,27 +1,21 @@
 package kr.or.bit.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.View;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.bit.model.Vote;
 
-@Controller
+@RestController
 @RequestMapping("/axios")
 public class AxiosContoller {
-	@Autowired
-	private View jsonView;
 	
 	@GetMapping("/addTodo")
-	public View addTodo(Model model) {
+	public Vote addTodo() {
 		System.out.println("애드투두");
 		Vote vote = new Vote();
 		vote.setComment_id(1);
 		vote.setUsername("권순조");
-		model.addAttribute("vote",vote);
-		return jsonView;
+		return vote;
 	}
 }
