1514d154429ad9a1b893f259d386b40fe2437a3d
LuiGee3471 <yjs3475@gmail.com>
2019-06-26 20:34:51 +0900

    채팅 제작 중

--- FinalProject/src/main/java/kr/or/bit/controller/SocketController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/SocketController.java
@@ -1,17 +1,30 @@
 package kr.or.bit.controller;
 
+import java.util.Set;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
+import kr.or.bit.dao.MemberDao;
 import kr.or.bit.model.Message;
 
 @Controller
 public class SocketController {
   @Autowired
   private SimpMessagingTemplate template;
+  
+  @Autowired
+  private SimpUserRegistry registry;
+  
+  @Autowired
+  private SqlSession sqlSession;
   
   @MessageMapping("/notification")
   @SendToUser("/queue/notification")
@@ -23,5 +36,17 @@ public class SocketController {
   public void sendAlarm(String hello) {
     System.out.println(hello);
     template.convertAndSendToUser("manager", "/queue/notice", "new notice");
+  }
+  
+  @MessageMapping("/chat")
+  public void sendMessage() {
+    Set<SimpUser> set = registry.getUsers();
+    
+    for (SimpUser s : set) {
+      MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+      System.out.println(memberDao.selectMemberByUsername(s.getName()).getName());
+    }
+    
+    template.convertAndSend("/topic/chat/group/1", set.size());
   }
 }
