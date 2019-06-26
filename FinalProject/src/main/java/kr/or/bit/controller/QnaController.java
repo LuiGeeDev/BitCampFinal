7a836a8b32b95275c42df95973745a856b6081c7
mhgyj0916 <mhgyj0916@gmail.com>
2019-06-26 20:33:59 +0900

    Qna 게시판 view 제작중

--- FinalProject/src/main/java/kr/or/bit/controller/QnaController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/QnaController.java
@@ -1,7 +1,5 @@
 package kr.or.bit.controller;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/qna")
 public class QnaController {
   
-  @Autowired
-  private SqlSession sqlsession;
+ 
   
   @GetMapping("/home")
   public String selectAllQna(Model model) {
