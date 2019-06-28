d669c4a9fa168b88592e1cfade47b56f55887cae
mhgyj0916 <mhgyj0916@gmail.com>
2019-06-28 17:10:15 +0900

    stack게시판 CRUD 연결중

--- FinalProject/src/main/java/kr/or/bit/controller/StackController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/StackController.java
@@ -1,5 +1,7 @@
 package kr.or.bit.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
@@ -9,6 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.or.bit.model.Article;
+import kr.or.bit.service.ArticleService;
+
 @Controller
 @RequestMapping("/stack")
 public class StackController {
@@ -16,16 +21,22 @@ public class StackController {
   @Autowired
   private SqlSession sqlsession;
   
+  @Autowired
+  ArticleService articleService;
+  
   //stack 메인으로 이동
   @GetMapping("/home")
-  public String selectAllStack(Model model,HttpServletRequest request) {
+  public String selectAllStack(Model model) {
+    List<Article> article = articleService.selectAllArticle(2);
+    model.addAttribute("stacklist",article);
     return "stack/home";
   }
   
   //stack 게시물 상세보기 버튼
   @GetMapping("/content")
-  public String selectStack(Model model) {
-    
+  public String selectStack(int id, Model model) {
+    Article article = articleService.selectOneArticle(id);
+    model.addAttribute("stackcontent",article);
     return "stack/content";
   }
   
