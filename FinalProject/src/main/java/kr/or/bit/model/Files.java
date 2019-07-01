2478b21b9cfd031960d24312f88c33d12ff13015
SunJoJang <gwon2091@gmail.com>
2019-07-01 18:28:01 +0900

    파일업로드 수정중

---
+++ FinalProject/src/main/java/kr/or/bit/model/Files.java
@@ -0,0 +1,29 @@
+package kr.or.bit.model;
+
+public class Files {
+  private int id;
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  private String original_filename;
+  private String filename;
+  public String getOriginal_filename() {
+    return original_filename;
+  }
+  public void setOriginal_filename(String original_filename) {
+    this.original_filename = original_filename;
+  }
+  public String getFilename() {
+    return filename;
+  }
+  public void setFilename(String filename) {
+    this.filename = filename;
+  }
+  @Override
+  public String toString() {
+    return "File [id=" + id + ", original_filename=" + original_filename + ", filename=" + filename + "]";
+  }
+}
