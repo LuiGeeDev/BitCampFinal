d81651a21453f30a3c97e97b874a85bc65a3b0d0
LuiGee3471 <yjs3475@gmail.com>
2019-07-02 12:07:51 +0900

    클래스 생성 완료

--- FinalProject/src/main/java/kr/or/bit/dao/ArticleDao.java
+++ FinalProject/src/main/java/kr/or/bit/dao/ArticleDao.java
@@ -39,4 +39,6 @@ public interface ArticleDao {
   int selectVote(@Param("id") int articleId, @Param("username") String username);
 
   void updateEable(int id);
+
+  List<Article> selectArticlesByPage(@Param("board_id") int board_id, @Param("page") int page);
 }
