06d4cf2fc36d68f11294d2cb07a38b32cc960b46
LuiGee3471 <yjs3475@gmail.com>
2019-07-04 19:21:57 +0900

    BoardController 수정 중

--- FinalProject/src/main/java/kr/or/bit/controller/BoardController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/BoardController.java
@@ -1,12 +1,11 @@
 package kr.or.bit.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,42 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import kr.or.bit.dao.ArticleDao;
-import kr.or.bit.dao.BoardDao;
-import kr.or.bit.dao.FilesDao;
-import kr.or.bit.dao.GeneralDao;
 import kr.or.bit.model.Article;
-import kr.or.bit.model.Board;
-import kr.or.bit.model.Files;
-import kr.or.bit.model.General;
 import kr.or.bit.service.BoardService;
-import kr.or.bit.utils.Helper;
+import kr.or.bit.service.FileUploadService;
 
 //@Controller
 @RequestMapping("/myclass/board")
 public class BoardController {
-  @Autowired
-  private SqlSession sqlSession;
   @Autowired
   private BoardService boardService;
 
   @GetMapping("/")
   public String listPage(int board_id, @RequestParam(defaultValue = "1") int page,
       @RequestParam(required = false) String sort, @RequestParam(required = false) String search, Model model) {
-    ArticleDao articleDao = sqlSession.getMapper(ArticleDao.class);
-    BoardDao boardDao = sqlSession.getMapper(BoardDao.class);
-    Board board = boardDao.selectBoardById(board_id);
-
     List<Article> articles = null;
     if (sort == null && search == null) {
-      articles = articleDao.selectArticlesByPage(board_id, (page - 1) * 20 + 1, page * 20);
+      articles = boardService.getArticlesByPage(board_id, page);
     } else if (sort != null) {
-
+      articles = boardService.getArticlesSorted(board_id, page, sort);
     } else if (search != null) {
-
+      articles = boardService.getArticlesBySearchWord(board_id, page, search);
     }
-  
-    model.addAttribute("board", board);
+
+    model.addAttribute("board", boardService.getBoardInfo(board_id));
     model.addAttribute("articles", articles);
 
     return null;
@@ -57,43 +43,18 @@ public class BoardController {
 
   @GetMapping("/write")
   public String writePage(int board_id, Model model) {
-    model.addAttribute("board_id", board_id);
+    model.addAttribute("board", boardService.getBoardInfo(board_id));
     return "myclass/general/write";
   }
 
   @PostMapping("/write")
-  public String writeArticle(Article article, List<MultipartFile> files) {
-    ArticleDao articleDao = sqlSession.getMapper(ArticleDao.class);
-    FilesDao filesDao = sqlSession.getMapper(FilesDao.class);
-    GeneralDao generalDao = sqlSession.getMapper(GeneralDao.class);
-
-    /*
-    Article INSERT
-    파일 INSERT
-    new General(),
-    General에 file ID 삽입
-    General INSERT
-    Article SELECT
-    */
-
-    return "redirect:/myclass/board/read?article_id=" + article.getId();
+  public String writeArticle(Article article, MultipartFile file1, MultipartFile file2, HttpServletRequest request) {
+    return "redirect:/myclass/board/read?article_id=" + boardService.writeArticle(article, file1, file2, request);
   }
 
   @GetMapping("/read")
   public String readArticle(int article_id, Model model) {
-    ArticleDao articleDao = sqlSession.getMapper(ArticleDao.class);
-    FilesDao filesDao = sqlSession.getMapper(FilesDao.class);
-    GeneralDao generalDao = sqlSession.getMapper(GeneralDao.class);
-
-    Article article = articleDao.selectOneArticle(article_id);
-    General general = generalDao.selectGeneralByArticleId(article_id);
-
-    List<Files> files = new ArrayList<>();
-    files.add(filesDao.selectFilesById(general.getFile1()));
-    files.add(filesDao.selectFilesById(general.getFile2()));
-    general.setFiles(files);
-    article.setOption(general);
-
+    Article article = boardService.readArticle(article_id);
     model.addAttribute("article", article);
 
     return null;
@@ -101,18 +62,7 @@ public class BoardController {
 
   @GetMapping("/update")
   public String updatePage(int article_id, Model model) {    
-    FilesDao filesDao = sqlSession.getMapper(FilesDao.class);
-    GeneralDao generalDao = sqlSession.getMapper(GeneralDao.class);
-    
     Article article = boardService.getArticleForUpdateOrDelete(article_id);
-
-    General general = generalDao.selectGeneralByArticleId(article_id);
-    List<Files> files = new ArrayList<>();
-    files.add(filesDao.selectFilesById(general.getFile1()));
-    files.add(filesDao.selectFilesById(general.getFile2()));
-    general.setFiles(files);
-    article.setOption(general);
-    
     model.addAttribute("article", article);
 
     return null;
@@ -120,13 +70,12 @@ public class BoardController {
 
   @PostMapping("/update")
   public String updateArticle(Article article, List<MultipartFile> files) {
-    boardService.updateArticle(article);
+    boardService.updateArticle(article, files);
     return "redirect:/myclass/board/read?article_id=" + article.getId();
   }
 
   @GetMapping("/delete")
   public String deleteArticle(int article_id) {
-    ArticleDao articleDao = sqlSession.getMapper(ArticleDao.class);
     Article article = boardService.getArticleForUpdateOrDelete(article_id);
     boardService.deleteArticle(article);
 
