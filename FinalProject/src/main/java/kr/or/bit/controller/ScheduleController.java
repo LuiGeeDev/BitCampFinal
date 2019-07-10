5df796951f6e64ff91992cc26d6f10152d1387b8
이힘찬 <glacks4871@gmail.com>
2019-07-10 17:46:37 +0900

    스케줄 컨트롤러, dao, mapper

--- FinalProject/src/main/java/kr/or/bit/controller/ScheduleController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/ScheduleController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.or.bit.dao.MemberDao;
 import kr.or.bit.dao.ScheduleDao;
+import kr.or.bit.model.Member;
 import kr.or.bit.model.Schedule;
+import kr.or.bit.utils.Helper;
 
 @Controller
 @RequestMapping("/myclass/schedule")
@@ -16,7 +19,6 @@ public class ScheduleController {
 
   @Autowired
   private SqlSession sqlSession;
-  
 
   @GetMapping("")
   public String selectTest() {
@@ -27,16 +29,22 @@ public class ScheduleController {
   @PostMapping("/insert")
   public String insertSchedule(Schedule schedule) {
     ScheduleDao scheduleDao = sqlSession.getMapper(ScheduleDao.class);
+    MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+    String username = Helper.userName();
+    Member user = memberDao.selectMemberByUsername(username);
+    schedule.setCourse_id(user.getCourse_id());
     scheduleDao.insertSchedule(schedule);
     return "myclass";
   }
+  
   @PostMapping("/update")
   public String updateSchedule(Schedule schedule, int id) {
     ScheduleDao scheduleDao = sqlSession.getMapper(ScheduleDao.class);
     scheduleDao.updateSchedule(schedule, id);
     return "myclass";
   }
-  @PostMapping("/delete")
+  
+  @GetMapping("/delete")
   public String deleteSchedule(int id) {
     ScheduleDao scheduledao = sqlSession.getMapper(ScheduleDao.class);
     scheduledao.deleteSchedule(id);
