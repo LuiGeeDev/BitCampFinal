0a964de0ef1b7965cd452c79f1edb6f5423c4215
mhgyj0916 <mhgyj0916@gmail.com>
2019-07-15 18:06:22 +0900

    mypage 비동기 도전

--- FinalProject/src/main/java/kr/or/bit/controller/MypageController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/MypageController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.bit.dao.ArticleDao;
@@ -65,10 +66,11 @@ public class MypageController {
     return "mypage/mypage";
   }
 
-  @GetMapping("/home/CommentList")
-  public String getCommentList(String user) {
-    
-    return null;
+  @PostMapping("/home/CommentList")
+  public @ResponseBody List<Comment> getCommentList(String user) {
+    CommentDao commentDao = sqlSession.getMapper(CommentDao.class);
+    List<Comment> comments = commentDao.selectAllCommentByUsername(user);
+    return comments;
   }
   
   
