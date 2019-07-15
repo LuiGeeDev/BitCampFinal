280f42efe54ac0e3a0e55abf5a4f6a61623ef322
BokDaBeen <ekqlszzzz@naver.com>
2019-07-15 19:31:55 +0900

    커밋

--- FinalProject/src/main/java/kr/or/bit/controller/VideoController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/VideoController.java
@@ -57,9 +57,7 @@ public class VideoController {
      * parameter로 받은 아이디 값을 이용, 해당하는 글을 불러와서 페이지에 글을 넘겨준다
      */
     Article article = articleService.selectOneArticle("video", id);
-    
     articleUpdateService.viewCount(article);
-    
     model.addAttribute("voteStatus", articleVoteService.selectVote(id, Helper.userName()));
     model.addAttribute("article", article);
     return "video/detail";
