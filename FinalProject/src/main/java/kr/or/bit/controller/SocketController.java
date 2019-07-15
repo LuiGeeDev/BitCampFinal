0533b0acff5f3b9d945a98176438a8347e905f3b
LuiGee3471 <yjs3475@gmail.com>
2019-07-15 16:44:32 +0900

    모여라 기능 구현

--- FinalProject/src/main/java/kr/or/bit/controller/SocketController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/SocketController.java
@@ -1,19 +1,20 @@
 package kr.or.bit.controller;
 
-import java.util.Set;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.user.SimpUser;
 import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Controller;
 
 import kr.or.bit.dao.MemberDao;
 import kr.or.bit.dao.NotificationDao;
 import kr.or.bit.model.ChatMessage;
+import kr.or.bit.model.Member;
 import kr.or.bit.model.Notification;
+import kr.or.bit.utils.Helper;
 
 @Controller
 public class SocketController {
@@ -26,22 +27,30 @@ public class SocketController {
   @Autowired
   private SqlSession sqlSession;
   
-  @MessageMapping({"/notice/vote", "/notice/comment", "/notice/reply", "/notice/message", "/notice/assemble"})
+  @MessageMapping({"/notice/vote", "/notice/comment", "/notice/reply", "/notice/message"})
   public void sendNotice(Notification notification) {
     NotificationDao notificationDao = sqlSession.getMapper(NotificationDao.class);
     notificationDao.insertNotification(notification);
     template.convertAndSendToUser(notification.getUsername(), "/queue/notice", notification);
   }
   
+  @MessageMapping("/notice/assemble")
+  public void sendAssembleNotice(Notification notification) {
+    MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+    NotificationDao notificationDao = sqlSession.getMapper(NotificationDao.class);
+    
+    Member teacher = memberDao.selectMemberByUsername(notification.getUsername());
+    List<Member> students = memberDao.selectAllMembersByMycourse(teacher.getCourse_id());
+    
+    for (Member student : students) {
+      template.convertAndSendToUser(student.getUsername(), "/queue/notice", notification);
+      notification.setUsername(student.getUsername());
+      notificationDao.insertNotification(notification);
+    }
+  }
+  
   @MessageMapping("/chat")
   public void sendMessage(ChatMessage chatMessage) {
-    Set<SimpUser> set = registry.getUsers();
-    
-    for (SimpUser s : set) {
-      MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
-      System.out.println(memberDao.selectMemberByUsername(s.getName()).getName());
-    }
-    
     template.convertAndSend("/topic/chat/groups/" + chatMessage.getGroup_id(), chatMessage);
   }
 }
