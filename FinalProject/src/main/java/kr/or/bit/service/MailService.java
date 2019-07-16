6dc7dcb3c8b9f56c9db45f03fcfb6305a5d43a8e
LuiGee3471 <yjs3475@gmail.com>
2019-07-16 21:00:10 +0900

    임시 비밀번호 발급 백엔드 구현

--- FinalProject/src/main/java/kr/or/bit/service/MailService.java
+++ FinalProject/src/main/java/kr/or/bit/service/MailService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import kr.or.bit.model.Member;
+
 @Service
 public class MailService {
   @Autowired
@@ -28,6 +30,22 @@ public class MailService {
     helper.setTo(to);
     helper.setSubject("[티밍] 강사 계정 생성 안내");
     String htmlContent = emailTemplateEngine.process("mail", context);
+    helper.setText(htmlContent, true);
+
+    mailSender.send(message);
+  }
+  
+  public void sendNewPasswordEmail(int newPassword, Member member) throws MessagingException {
+    Context context = new Context();
+    context.setVariable("member", member);
+    
+    String from = "mail@bitcamp.com";
+    MimeMessage message = mailSender.createMimeMessage();
+    MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+    helper.setFrom(from);
+    helper.setTo(member.getEmail());
+    helper.setSubject("[티밍] 임시 비밀번호 발급");
+    String htmlContent = emailTemplateEngine.process("passwordMail", context);
     helper.setText(htmlContent, true);
 
     mailSender.send(message);
