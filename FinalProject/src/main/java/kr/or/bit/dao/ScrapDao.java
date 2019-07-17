935019a1288ee389000eabd3ed37e9a651dfb72b
mhgyj0916 <mhgyj0916@gmail.com>
2019-07-17 18:27:32 +0900

    scrapdao

--- FinalProject/src/main/java/kr/or/bit/dao/ScrapDao.java
+++ FinalProject/src/main/java/kr/or/bit/dao/ScrapDao.java
@@ -17,8 +17,14 @@ import kr.or.bit.model.Article;
 */
 public interface ScrapDao {
   
+  //게시판에서 스크랩버튼 눌렀을때 쓰는 메소드
   void insertScrap(@Param("article_id") int article_id, @Param("username") String username);
   
+  //마이페이지 스크랩목록 불러올때 쓰는 메소드
   List<Article> selectAllScrap(String username);
+  
+  //마이페이지 스크랩목록에서 삭제하고싶을때 쓰세요
+  void deleteScrap(int article_id);
+  
   
 }
