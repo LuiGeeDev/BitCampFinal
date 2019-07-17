c36ccb95da1914d6422ba46190f3ad679047b8b6
BokDaBeen <ekqlszzzz@naver.com>
2019-07-17 10:03:10 +0900

    timeline update

--- FinalProject/src/main/java/kr/or/bit/controller/ProjectController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/ProjectController.java
@@ -1,7 +1,9 @@
 package kr.or.bit.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +14,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.bit.dao.ChecklistDao;
 import kr.or.bit.dao.GroupDao;
+import kr.or.bit.dao.MemberDao;
 import kr.or.bit.dao.TimelineDao;
 import kr.or.bit.model.Checklist;
 import kr.or.bit.model.Group;
 import kr.or.bit.model.Timeline;
 import kr.or.bit.utils.Helper;
-
 @Controller
 @RequestMapping("myclass/project")
 public class ProjectController {
@@ -32,6 +36,10 @@ public class ProjectController {
   public String projectPage(int group_id, Model model) {
     GroupDao groupDao = sqlSession.getMapper(GroupDao.class);
     ChecklistDao checklistDao = sqlSession.getMapper(ChecklistDao.class);
+    TimelineDao timelineDao = sqlSession.getMapper(TimelineDao.class);
+    MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+    List<Timeline> timelineList = timelineDao.selectTimelineByGroupId(group_id);
+    
     Group group = groupDao.selectGroupById(group_id);
     
     List<Checklist> checklist = checklistDao.selectAllChecklist(group_id);
@@ -40,8 +48,13 @@ public class ProjectController {
       checklistContents.add(todo.getContent());
     }
     
+    for(Timeline timeline : timelineList) {
+      timeline.setWriter(memberDao.selectMemberByUsername(timeline.getUsername()));
+    }
+    
     model.addAttribute("group", group);
     model.addAttribute("checklist",checklist);
+    model.addAttribute("timelineList",timelineList);
     return "myclass/project/main";
   }
 
@@ -54,24 +67,5 @@ public class ProjectController {
     return "myclass/chat/main";
   }
   
-  @PostMapping("/checkUpdate")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  @Transactional
-  public void checkUpdate(int id, int group_id) {
-    ChecklistDao checklistDao = sqlSession.getMapper(ChecklistDao.class);
-    TimelineDao timelineDao = sqlSession.getMapper(TimelineDao.class);
-    checklistDao.checkUpdate(id, Helper.userName());
-    
-    Checklist checklist = checklistDao.selectOneChecklist(id);
 
-    if(checklist.getChecked() == 0 ) {
-      return;
-    }
-    Timeline timeline = new Timeline();
-    timeline.setTitle("체크리스트 완료");
-    timeline.setContent(checklist.getContent());
-    timeline.setGroup_id(group_id);
-    timeline.setUsername(Helper.userName());
-    timelineDao.insertTimeline(timeline);
-  }
 }
