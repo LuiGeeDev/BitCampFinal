ee46de05519c7ca1751549334584c02e1bec6ce5
LuiGee3471 <yjs3475@gmail.com>
2019-07-18 14:58:44 +0900

    알림 기능 수정 중

--- FinalProject/src/main/webapp/js/common.js
+++ FinalProject/src/main/webapp/js/common.js
@@ -12,18 +12,20 @@ function addNotice(notification) {
   const link = $(`<a class="notice-link" href="${notification.link}">`);
   const notice = $("<div class='notice'>");
   const noticeTitle = $(`<h6 class="notice-title">${notification.title}</h6>`);
-  const noticeContent = $(`<p class="notice-content">${notification.content}</p>`);
+  const noticeContent = $(
+    `<p class="notice-content">${notification.content}</p>`
+  );
   notice.append(noticeTitle).append(noticeContent);
   link.append(notice);
-  
+
   if ($(".notice").length === 20) {
     $(".notice")[19].remove();
   }
-  
+
   if ($(".no-notice")) {
     $(".no-notice").remove();
   }
-  
+
   $("#notices").prepend(link);
   $(".noti-count").show();
   $(".noti-count").text(Number($(".noti-count").text()) + 1);
@@ -32,18 +34,26 @@ function addNotice(notification) {
 function showToast(notification) {
   const toastPositioner = $(".toast-positioner");
   const link = $(`<a href="${notification.link}">`);
-  const toast = $(`<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="5000">`);
+  const toast = $(
+    `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="5000">`
+  );
   const toastHeader = $(`<div class="toast-header">`);
   const toastBody = $(`<div class="toast-body">`);
-  const toastTitle = $(`<strong class="mr-auto">${notification.title}</strong>`);
-  const toastContent = $(`<p class="toast-content">${notification.content}</p>`);
-  const closeButton = $(`<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close"><span aria-hidden="true">&times;</span></button>`);
+  const toastTitle = $(
+    `<strong class="mr-auto">${notification.title}</strong>`
+  );
+  const toastContent = $(
+    `<p class="toast-content">${notification.content}</p>`
+  );
+  const closeButton = $(
+    `<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close"><span aria-hidden="true">&times;</span></button>`
+  );
   toastHeader.append(toastTitle).append(closeButton);
   toastBody.append(toastContent);
   link.append(toastBody);
   toast.append(toastHeader).append(link);
   toastPositioner.append(toast);
-  $(".toast-positioner .toast:last-child").toast('show');
+  $(".toast-positioner .toast:last-child").toast("show");
 }
 
 // 추천 알림
@@ -70,7 +80,7 @@ function sendCommentNotice(senderName, receiverUsername, link) {
   client.send("/app/notice/comment", {}, JSON.stringify(notification));
 }
 
-// 
+//
 function sendReplyNotice(senderName, receiverUsername, link) {
   const notification = {
     title: "답글",
@@ -95,11 +105,12 @@ function sendAssembleNotice(senderName, username, link) {
 
 function sendMessageNotice(senderName, receiverUsername) {
   const notification = {
-      title: "쪽지",
-      username: receiverUsername,
-      content: `${senderName}님이 쪽지를 보냈습니다`,
-      link: "/message"
-  }
+    title: "쪽지",
+    username: receiverUsername,
+    content: `${senderName}님이 쪽지를 보냈습니다`,
+    link: "/message"
+  };
 
+  $(".message-count").val(Number($(".message-count").val()) + 1);
   client.send("/app/notice/message", {}, JSON.stringify(notification));
 }
