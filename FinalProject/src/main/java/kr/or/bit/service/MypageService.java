40d20bdab733bd7ab87f32590dbe53bd191caf87
mhgyj0916 <mhgyj0916@gmail.com>
2019-07-19 10:50:41 +0900

    마이페이지수정ing

--- FinalProject/src/main/java/kr/or/bit/service/MypageService.java
+++ FinalProject/src/main/java/kr/or/bit/service/MypageService.java
@@ -1,5 +1,7 @@
 package kr.or.bit.service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,11 +12,22 @@ import org.springframework.stereotype.Service;
 
 import kr.or.bit.dao.ArticleDao;
 import kr.or.bit.dao.BoardDao;
+import kr.or.bit.dao.CommentDao;
+import kr.or.bit.dao.CourseDao;
 import kr.or.bit.dao.MemberDao;
+import kr.or.bit.dao.MypageDao;
+import kr.or.bit.dao.QnaDao;
 import kr.or.bit.dao.ScrapDao;
+import kr.or.bit.dao.StackDao;
 import kr.or.bit.model.Article;
+import kr.or.bit.model.ArticleOption;
 import kr.or.bit.model.Board;
+import kr.or.bit.model.Comment;
+import kr.or.bit.model.Course;
 import kr.or.bit.model.Member;
+import kr.or.bit.model.Tag;
+import kr.or.bit.utils.Helper;
+import kr.or.bit.utils.Pager;
 
 @Service
 public class MypageService {
@@ -28,6 +41,42 @@ public class MypageService {
       article.setTimeLocal(article.getTime().toLocalDateTime());
       // article.setUpdatedTimeLocal(article.getUpdated_time().toLocalDateTime());
     }
+    return articleList;
+  }
+  
+  public List<Article> selectAllMyArticlesByUsername(Pager pager,String username) {
+/*    StackDao stackdao = sqlSession.getMapper(StackDao.class);
+    CommentDao commentdao = sqlSession.getMapper(CommentDao.class);
+    MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+    QnaDao qnaDao = sqlSession.getMapper(QnaDao.class);
+    List<Article> articlelist = stackdao.selectAllStackArticle(pager);
+    
+    for (Article article : articlelist) {
+      ArticleOption option = null;
+      option = qnaDao.selectQnaByArticleId(article.getId());
+      List<Comment> commentList = commentdao.selectAllComment(article.getId());
+      List<Tag> taglist = stackdao.selectTagList(article.getId()); 
+      for (Comment comment : commentList) {
+        comment.setWriter(memberDao.selectMemberByUsername(comment.getUsername()));
+      }
+      article.setOption(option);
+      article.setTags(taglist);
+      article.setCommentlist(commentList);
+      article.setTimeLocal(article.getTime().toLocalDateTime());
+      article.setWriter(memberDao.selectMemberByUsername(article.getUsername()));   
+    }
+    return articlelist;*/
+    
+    
+    MypageDao mypageDao = sqlSession.getMapper(MypageDao.class);
+    CommentDao commentdao = sqlSession.getMapper(CommentDao.class);
+    List<Article> articleList = mypageDao.selectEnableArticleByUsername2(pager,username);
+    for (Article article : articleList) {
+      article.setTimeLocal(article.getTime().toLocalDateTime());
+      List<Comment> commentList = commentdao.selectAllComment(article.getId());
+      article.setCommentlist(commentList);
+    }
+    
     return articleList;
   }
 
@@ -83,8 +132,38 @@ public class MypageService {
     return Scrap;
   }
   
+  public int coursePeriod (String username) {
+    MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+    CourseDao courseDao = sqlSession.getMapper(CourseDao.class);
+    Member user = memberDao.selectMemberByUsername(username);
+    Course course = courseDao.selectCourse(user.getCourse_id());
+    course.setEndDate(course.getEnd_date().toLocalDate());
+    course.setStartDate(course.getStart_date().toLocalDate());
+    Period diff = Period.between(course.getStartDate(), course.getEndDate());
+    Period diff2 = Period.between(course.getStartDate(), LocalDate.now());
+    int completion = Math.round((float) diff2.getDays() / diff.getDays() * 100);
+    
+    return completion;
+  }
   
-  
-  
+  public List<Article> selectMyArticlesByboardSearch(Pager pager,String boardSearch,String criteria,String username) {
+    MypageDao mypageDao = sqlSession.getMapper(MypageDao.class);
+    CommentDao commentdao = sqlSession.getMapper(CommentDao.class);
+    MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+    List<Article> articleList = null;
+    if(criteria.equals("titleOrContent")) {
+      articleList = mypageDao.selectMyArticleByTitleOrContent(pager,boardSearch, username);
+    }else if(criteria.equals("title")) {
+      articleList = mypageDao.selectMyArticleByTitle(pager,boardSearch, username);
+    }else {
+      articleList = mypageDao.selectMyArticleByWriter(pager,boardSearch, username);
+    }
+    
+    for (Article article : articleList) {
+      article.setTimeLocal(article.getTime().toLocalDateTime());
+      article.setWriter(memberDao.selectMemberByUsername(article.getUsername()));   
+    }
+    return articleList;
+  }
 
 }
