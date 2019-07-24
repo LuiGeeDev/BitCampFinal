bd0eb72da4f689dbf95eda36d0a0545bd1f2cd47
이힘찬 <glacks4871@gmail.com>
2019-07-24 16:12:48 +0900

    게시판 추가 오류 수정

--- FinalProject/src/main/java/kr/or/bit/controller/MyClassController.java
+++ FinalProject/src/main/java/kr/or/bit/controller/MyClassController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.bit.dao.ArticleDao;
@@ -382,6 +384,7 @@ public class MyClassController {
   }
 
   @PostMapping("/create/board")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public void createBoard(@RequestBody BoardAddRemove boardAddRemove) {
     boardService.decideBoardAddOrRemove(boardAddRemove);
   }
@@ -590,8 +593,6 @@ public class MyClassController {
     }
 
     Article homeworkarticle = articledao.selectRecentHomework(username);
-    System.out.println("homeworkArticle : "+articledao.selectRecentHomework(username));
-    System.out.println("homeworkArticle : "+homeworkarticle);
     if(homeworkarticle != null) {
       Homework homework = homeworkdao.selectHomeworkByArticleId(homeworkarticle.getId());
       homeworkarticle.setOption(homework);
